7be93f92ded5eab10bd117a3cb9643a39def0047
Fransuelton <elton6103@gmail.com>
2025-07-09 19:50:23 -0300

    ✨ feat: add barrel export index file to export components from layout

---
+++ src/components/index.jsx
@@ -0,0 +1,7 @@
+export { Header } from "./layout/header";
+export { Main } from "./layout/main";
+export { About } from "./layout/about";
+export { Skills } from "./layout/skills";
+export { Footer } from "./layout/footer";
+export { Contact } from "./layout/contact";
+export { Projects } from "./layout/projects";
