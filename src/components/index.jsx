7e96a6dc1476012c859658282ca742b75a4f271f
Fransuelton <elton6103@gmail.com>
2025-07-13 06:53:02 -0300

    🔥 remove: eliminate Skills component export from index.jsx

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,7 +1,6 @@
 export { Header } from "./layout/header";
 export { Main } from "./layout/main";
 export { About } from "./layout/about";
-export { Skills } from "./layout/skills";
 export { Footer } from "./layout/footer";
 export { Contact } from "./layout/contact";
 export { Projects } from "./layout/projects";
